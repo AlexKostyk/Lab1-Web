6c5e023a2162ceeef38def268a2e1ed2382631ef
polyd <polydany@mail.ru>
2022-11-01 16:51:47 +0300

    a

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <title>Лабораторная работа по Web-программированию №1</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+
+<body>
+
+
+
+<div class="box">
+<h2>
+    Костюк Александр, Поляков Даниил<br>Группа P33212 <br>Вариант: 16213
+</h2>
+
+
+
+
+
+<img
+        src="image.png"
+        alt="Не удалось не получилось">
+</div>
+</body>
+
+
+</html>
